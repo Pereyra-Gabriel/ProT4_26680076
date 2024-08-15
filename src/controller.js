d22c9f0ed490eeb8e9eaa6e3a3c37a0be1641d77
gabri <pgabri.78@gmail.com>
2024-08-14 21:59:05 -0300

    Se agrego la funcionalidad de Ingresar (add) y eliminar (delete) libro de la base de datos

--- src/controller.js
+++ src/controller.js
@@ -12,6 +12,12 @@ class LibrosController{
         const [result] = await pool.query(`INSERT INTO Libros(nombre, autor, categoria, año, ISBN) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.año, libro.ISBN]);
         res.json({"Id insertado": result.insertId});
     }
+
+    async delete(req, res){
+        const libro = req.body;
+        const [result] = await pool.query(`DELETE FROM Libros WHERE ISBN=(?)`, [libro.ISBN]);
+        res.json({"Registros eliminados": result.affectedRows});
+    }
 }
 
 export const libro = new LibrosController();
