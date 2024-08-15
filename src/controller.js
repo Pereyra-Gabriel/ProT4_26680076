b9af8a6a8d6d4a090d4f081d2c38165dd5734c6d
gabri <pgabri.78@gmail.com>
2024-08-15 00:11:14 -0300

    Se agregaron las funcionalidades de Actualizar y buscar un libro mediante el ID

--- src/controller.js
+++ src/controller.js
@@ -18,6 +18,22 @@ class LibrosController{
         const [result] = await pool.query(`DELETE FROM Libros WHERE ISBN=(?)`, [libro.ISBN]);
         res.json({"Registros eliminados": result.affectedRows});
     }
-}
 
+    async update(req, res){
+        const libro = req.body;
+        const [result] = await pool.query(`UPDATE Libros SET nombre=(?), autor=(?), categoria=(?), año=(?), ISBN=(?) WHERE id=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.año, libro.isbn, libro.id]);
+        res.json({"Registros actualizados": result.changedRows});
+    }
+
+    async getOne(req, res){
+        const libro = req.body;
+        const [result] = await pool.query(`SELECT * FROM Libros WHERE id=(?)`, [libro.id]);
+
+        if (result.lenght === 0){
+            res.json({"Libro no encontrado": result.length === 0});
+        } else {
+        res.json(result);
+        }
+    }
+}
 export const libro = new LibrosController();
